ea2f9ccf2a726a1fb63ace8e5f1ddd7d236acc01
Laurent Cerveau <lcerveau@mmyneta.com>
2016-11-03 10:59:40 +0100

    Initial Commit

---
+++ libSVG/libSVG.h
@@ -0,0 +1,19 @@
+//
+//  libSVG.h
+//  libSVG
+//
+//  Created by Laurent Cerveau on 11/3/16.
+//  Copyright © 2016 MMyneta. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for libSVG.
+FOUNDATION_EXPORT double libSVGVersionNumber;
+
+//! Project version string for libSVG.
+FOUNDATION_EXPORT const unsigned char libSVGVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libSVG/PublicHeader.h>
+
+
