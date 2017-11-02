da0d9738ca587644d33e24cea0f2c86011f8cb7e
Laurent Cerveau <lcerveau@mmyneta.com>
2017-11-02 08:23:30 +0100

    [core] work on core infrasrtucture

---
+++ libSVG/SVGRendererCoreGraphics.swift
@@ -0,0 +1,17 @@
+//
+//  SVGRenderer.swift
+//  libSVG
+//
+//  Created by Laurent Cerveau on 04/14/2017.
+//  Copyright © 2017 MMyneta. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+class SVGRenderer {
+    var identifier:String?
+    var drawPort:Any?
+
+
+}
